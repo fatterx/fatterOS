be692fb0321d18c9967c2fd566e66a5fec62c7c3
fatter <lwn888@gmail.com>
2012-03-13 14:29:28 +0800

    format the css files

--- js/fatterOS.js
+++ js/fatterOS.js
@@ -360,7 +360,6 @@ $(document).ready(function(){
 
 			$(document).bind("mousemove.fw_dnd",function(e){		
 				e.preventDefault(); 
-				e.stopPropagation();
 				onMove(e);
 			});
 
@@ -450,11 +449,8 @@ $(document).ready(function(){
 			});
 
 			$(document).bind("mousemove.fw_resize",function(e){
-
 					e.preventDefault(); 
-					e.stopPropagation();
 					onResize(e);
-
 			});
 
 			$(document).bind("mouseup.fw_resize",function(e){
@@ -698,12 +694,6 @@ $(document).ready(function(){
 			title: 			'filemgr',
 			width: 			800,
 			height: 		500,
-			cookie: 		{
-								expires : 30,
-								domain  : '',
-								path    : '/',
-								secure  : false
-							},
 			toolbar: 		[
 								['reload', 'mkdir', 'mkfile', 'upload'],
 								['open file disabled', 'copy file disabled', 'paste disabled', 'rm file disabled'],
@@ -724,27 +714,27 @@ $(document).ready(function(){
 		};
 
 		this.options = $.extend({}, this.defaults, options || {});
-		this.elem_shortcut_button = $("<ul class=\"shortcut-button\" />");
-		this.elem_clear_div = $("<div class=\"clear\" />");
-		this.elem_address = $("<div class=\"address\"><span>地址</span><div title=\"back\" id=\"back_button\" class=\"disabled\"/><div title=\"forword\" id=\"forword_button\" class=\"disabled\"/><div title=\"Go\" id=\"go_button\"/><div class=\"input-border\"><input type=\"text\" id=\"url_text\"/></div></div>");
-		this.elem_file_tree = $("<ul class=\"file-tree\" />");
-		this.elem_left_nav_tree = $("<div class=\"left-nav-tree\" />").append(this.elem_file_tree);
-		this.elem_filemgr_tag = $("<div class=\"filemgr-tag\"><span class=\"tag-scroll-left tag-scroll\">&lt;</span><ul class=\"tag\"></ul><span class=\"tag-scroll-right tag-scroll\">&gt;</span></div>");
-		this.elem_filemgr_resize = $("<div class=\"filemgr-resize\" />");
-		this.elem_filemgr_content_box = $("<div class=\"filemgr-content-box\" />");
-		this.elem_state_bar = $("<div class=\"filemgr-state-bar\" />");
-		this.elem_filemgr_content_wrap = $("<div class=\"filemgr-content-wrap frame-current\" />")
-								.append(this.elem_filemgr_tag)
-								.append(this.elem_filemgr_content_box);
+		this.$shortcut_button = $("<ul class=\"shortcut-button\" />");
+		this.$clear_div = $("<div class=\"clear\" />");
+		this.$address = $("<div class=\"fwindow-address\"><span>地址</span><div title=\"back\" id=\"back_button\" class=\"disabled\"/><div title=\"forword\" id=\"forword_button\" class=\"disabled\"/><div title=\"Go\" id=\"go_button\"/><div class=\"input-body\"><input type=\"text\" id=\"url_text\"/></div></div>");
+		this.$file_tree = $("<ul class=\"file-tree\" />");
+		this.$left_nav_tree = $("<div class=\"left-nav-tree\" />").append(this.$file_tree);
+		this.$filemgr_tag = $("<div class=\"filemgr-tag\"><span class=\"tag-scroll-left tag-scroll\">&lt;</span><ul class=\"tag\"></ul><span class=\"tag-scroll-right tag-scroll\">&gt;</span></div>");
+		this.$filemgr_resize = $("<div class=\"filemgr-resize\" />");
+		this.$filemgr_content_box = $("<div class=\"filemgr-content-box\" />");
+		this.$status_bar = $("<div class=\"filemgr-status-bar\" />");
+		this.$filemgr_content_wrap = $("<div class=\"filemgr-content-wrap frame-current\" />")
+								.append(this.$filemgr_tag)
+								.append(this.$filemgr_content_box);
 		this.target = $("<div class=\"filemgr-wrap\" />")
-								.append(this.elem_shortcut_button)
-								.append(this.elem_clear_div)
-								.append(this.elem_address)
-								.append(this.elem_left_nav_tree)
-								.append(this.elem_filemgr_resize)
-								.append(this.elem_filemgr_content_wrap)
+								.append(this.$shortcut_button)
+								.append(this.$clear_div)
+								.append(this.$address)
+								.append(this.$left_nav_tree)
+								.append(this.$filemgr_resize)
+								.append(this.$filemgr_content_wrap)
 								.append("<div class=\"clear\" />")
-								.append(this.elem_state_bar);
+								.append(this.$status_bar);
 		this.window = new fatterOS.window(this.options).setContent(this.target);
 		this.init();	
 	};
@@ -766,7 +756,7 @@ $(document).ready(function(){
 				for ( ; i < lengths; i++ ) {
 
 					if ( isSplit ) {
-						this.elem_shortcut_button.append('<li class="split" />');
+						this.$shortcut_button.append('<li class="split" />');
 					}
 					isSplit = false;
 
@@ -776,7 +766,7 @@ $(document).ready(function(){
 						if (n) {
 							isSplit = true;
 							$('<li class="'+n+'" title="'+n+'" name="'+n+'"/>')
-								.appendTo(this.elem_shortcut_button)
+								.appendTo(this.$shortcut_button)
 								.bind('click',function() {
 									var cmd = this.title.split(" ")[0];
 									!$(this).hasClass('disabled') && (fatterOS.system[cmd])();
@@ -789,7 +779,7 @@ $(document).ready(function(){
 				}
 				
 				this.getFileTree();
-				this.resizeBind(this.elem_filemgr_resize,this.elem_left_nav_tree);
+				this.resizeBind(this.$filemgr_resize,this.$left_nav_tree);
 				this.eventBind();
 				this.dragSelect();
 				this.adjustWindow();
@@ -799,8 +789,8 @@ $(document).ready(function(){
 			resizeBind: function( resizeHandle ){
 
 				var self = this,
-					left_nav_tree = self.elem_left_nav_tree,
-					left_frame = self.elem_filemgr_content_wrap;
+					left_nav_tree = self.$left_nav_tree,
+					left_frame = self.$filemgr_content_wrap;
 
 				( function( self ){
 
@@ -812,7 +802,6 @@ $(document).ready(function(){
 
 					$(document).bind("mousemove.frame_resize",function(e){
 						e.preventDefault(); 
-						e.stopPropagation();
 						if( self.onResizeLeftSide ){
 								onResizeLeft(e);
 						} else {
@@ -881,13 +870,13 @@ $(document).ready(function(){
 				var windowSize = this.window.getSize(),
 					curWidth  = windowSize.width,
 					curHeight = windowSize.height,
-					contentWrapHeight = curHeight - this.elem_address.height() - this.elem_shortcut_button.height() - this.elem_state_bar.height() - 2 - 10,//减去address 35px shortcut 30px 2px border;
+					windowTitle = 35,
+					contentWrapHeight = curHeight - this.$address.height() - this.$shortcut_button.height() - this.$status_bar.height() - windowTitle,
 					contentWrapWidth = 0,
-					contentBoxHeight = contentWrapHeight - this.elem_filemgr_tag.height();
-
+					contentBoxHeight = contentWrapHeight - this.$filemgr_tag.height();
 					if( this.target.data("twice") === true ){
 					
-						contentWrapWidth=(curWidth-this.elem_left_nav_tree.width())/2-6;  //减去padding 5，左边导航 双显
+						contentWrapWidth=(curWidth-this.$left_nav_tree.width())/2-6;  //减去padding 5，左边导航 双显
 						this.target.find(".filemgr-content-wrap").css({"width":contentWrapWidth,"float":"left"});
 					
 					} else {
@@ -895,7 +884,7 @@ $(document).ready(function(){
 					}
 
 					this.target.css({height:curHeight});
-					this.elem_left_nav_tree.css("height",contentWrapHeight);
+					this.$left_nav_tree.css("height",contentWrapHeight);
 					this.target.find(".filemgr-content-wrap").css("height",contentWrapHeight);
 					this.target.find(".filemgr-content-box").css("height",contentBoxHeight);
 					this.target.find(".filemgr-resize").css("height",contentBoxHeight);
@@ -903,27 +892,17 @@ $(document).ready(function(){
 
 			updateShortcutButton: function(target){
 		
-					var buttons = this.elem_shortcut_button;
-					var fileButtons = buttons.find(".file");
-		//			var executButton = ['open','copy','rm','rename','edit','info'];
-	//				var target = this.target.find(".frame-current .content-cur .selected");
-					
-		//			executButton.push(mkdir);
-		//
-					if(target){
-							
-						fileButtons.removeClass("disabled");
-					
-					}else{
-				
-						fileButtons.addClass("disabled");
+					var buttons = this.$shortcut_button,
+						fileButtons = buttons.find(".file");
 
+					if(target){
+						fileButtons.removeClass("disabled");
+					} else {
+						fileButtons.addClass("disabled");
 					}
 
 					if(fatterOS.cache.clipBoard.length !== 0 ){
-					
 						buttons.find(".paste").removeClass("disabled");
-					
 					}
 							 
 			},
@@ -934,18 +913,18 @@ $(document).ready(function(){
 					hashList = data.hashList,
 					hashNo = data.hashNo;
 
-				this.elem_address.find("#back_button").attr("className","disabled");
-				this.elem_address.find("#forword_button").attr("className","disabled");
+				this.$address.find("#back_button").attr("className","disabled");
+				this.$address.find("#forword_button").attr("className","disabled");
 
 				if(hashList.length>1){
 
 					if(hashNo>0){
 
-						this.elem_address.find("#back_button").removeClass("disabled");
+						this.$address.find("#back_button").removeClass("disabled");
 					}
 					
 					if(hashNo<(hashList.length-1)){
-						this.elem_address.find("#forword_button").removeClass("disabled");
+						this.$address.find("#forword_button").removeClass("disabled");
 					}
 
 				}
@@ -991,10 +970,10 @@ $(document).ready(function(){
 						dataType:'json',
 						success:function(d){
 							$.each(d,function(i){
-								$("<li>"+d[i].name+"</li>").appendTo(self.elem_file_tree)
+								$("<li>"+d[i].name+"</li>").appendTo(self.$file_tree)
 														   .bind("click",function(e){
 																e.stopPropagation();
-																self.elem_file_tree.find("li").removeClass("selected");
+																self.$file_tree.find("li").removeClass("selected");
 																$(this).addClass("selected");
 																self.showFile("/"+fatterOS.tools.encodeChinese(this.innerHTML),this.innerHTML);
 								});
@@ -1010,8 +989,8 @@ $(document).ready(function(){
 					tag_ul = self.target.find(".frame-current ul.tag"),
 					target = target || "_blank",
 					cur_tag_id = "tag_li_" + fatterOS.filemgr.id,
-					$backBtn = self.elem_address.find("#back_button"),
-					$forwordBtn = self.elem_address.find("#forword_button"),
+					$backBtn = self.$address.find("#back_button"),
+					$forwordBtn = self.$address.find("#forword_button"),
 					files_html = "",
 					tabs = fatterOS.cache.tabs,
 					hashNo = 0,
@@ -1027,7 +1006,7 @@ $(document).ready(function(){
 						dataType:"json",
 						success:function(data){
 							$.each(data,function(i){		
-								files_html += "<div class=\"docs-list\"><div class=\"file-"+data[i].type+"\"><img src=\"images/ext/"+data[i].type+".png\" alt=\"\" /><input type=\"text\" value="+data[i].name+" class=\"rename-text\" /></div></div>";
+								files_html += "<div data-name="+data[i].name+" class=\"docs-list\"><div class=\"file-"+data[i].type+"\"><img src=\"images/ext/"+data[i].type+".png\" alt=\"\" /><input type=\"text\" value="+data[i].name+" class=\"rename-text\" /></div></div>";
 								fileName.push(data[i].name);
 							});
 
@@ -1062,8 +1041,8 @@ $(document).ready(function(){
 								};
 
 							} else {
-								var cur_tag_li = self.target.find(".frame-current .tag-cur"),
-									cur_content = self.target.find(".frame-current .content-cur");
+								var cur_tag_li = self.target.find(".frame-current .tag-current"),
+									cur_content = self.target.find(".frame-current .content-current");
 
 								cur_tag_id = cur_tag_li.attr("id");	
 								//hashList = cur_content.data("data").hashList;
@@ -1094,7 +1073,7 @@ $(document).ready(function(){
 
 								cur_tag_li.children("a").html(title);
 								self.setAddress(url);
-								self.showState(target);
+								self.showStatus(target);
 								self.checkBackForwordButton(cur_tag_id);
 
 							}
@@ -1149,12 +1128,12 @@ $(document).ready(function(){
 				return fatterOS.cache.tabs[tag_id].hashList[0].url;
 			},
 			//状态栏显示
-			showState: function(tag_id){
+			showStatus: function(tag_id){
 				var source = $("#content_"+tag_id+">div.docs-list"),
-					state = $(".state-bar>span"),
+					status = $(".status-bar>span"),
 					s = "";
 				s += "共" + source.length + "项";
-				state.html(s);	
+				status.html(s);	
 			},
 			//切换标签
 			switchTag: function(tag_id){
@@ -1164,12 +1143,12 @@ $(document).ready(function(){
 					tags_lis = this.target.find(".frame-current ul.tag>li"),
 					address = this.getHideAddress(tag_id);
 
-				tags_lis.removeClass("tag-cur");
-				contents.removeClass("content-cur");
-				content_cur.addClass("content-cur");
-				tag_li_cur.addClass("tag-cur").css("display","block");
+				tags_lis.removeClass("tag-current");
+				contents.removeClass("content-current");
+				content_cur.addClass("content-current");
+				tag_li_cur.addClass("tag-current").css("display","block");
 				this.setAddress(address);
-				this.showState(tag_id);
+				this.showStatus(tag_id);
 				this.checkBackForwordButton(tag_id);
 
 			},
@@ -1207,11 +1186,7 @@ $(document).ready(function(){
 				var url=str.split("/");
 				return fatterOS.tools.decodeChinese(url[url.length-1]);
 			},
-/*
-			getSelf:function(){
-				return this.target;
-			},
-*/
+			
 			selectTarget : function($target){
 				var name = $target.find("input").val();
 
@@ -1221,7 +1196,7 @@ $(document).ready(function(){
 			},
 
 			selectAllTarget: function(){
-				var $targets = this.target.find(".frame-current .content-cur div.docs-list");
+				var $targets = this.target.find(".frame-current .content-current div.docs-list");
 				
 				$targets.addClass("selected");
 				$.each($targets,function(i){
@@ -1240,7 +1215,7 @@ $(document).ready(function(){
 			},
 
 			unSelectAllTarget: function(){
-				this.target.find(".frame-current .content-cur div.docs-list").removeClass("selected");
+				this.target.find(".frame-current .content-current div.docs-list").removeClass("selected");
 				fatterOS.cache.select = [];
 				this.updateShortcutButton();
 			},
@@ -1256,7 +1231,7 @@ $(document).ready(function(){
 				} else {
 
 					this.target.data("twice",true);	
-					this.elem_filemgr_content_wrap
+					this.$filemgr_content_wrap
 							.removeClass("frame-current")
 							.after("<div class=\"filemgr-resize dbframe\" /><div class=\"filemgr-content-wrap frame-current right-frame\"><div class=\"filemgr-tag\"><ul class=\"tag\"><li class=\"tag-scroll-left tag-scroll\">&lt;</li><li class=\"tag-scroll-right tag-scroll\">&gt;</li></ul></div><div class=\"filemgr-content-box\" /></div>");
 					
@@ -1314,8 +1289,6 @@ $(document).ready(function(){
 					});
 
 					$(document).bind("mousemove.drag_select",function(e){
-						e.stopPropagation();
-						e.preventDefault();
 						onSelect(e);
 					});
 
@@ -1353,7 +1326,7 @@ $(document).ready(function(){
 						selectDiv.css({"left":left,"top":top,"width":width,"height":height});
 						selectDiv.show();
 
-						var targets = self.target.find(".content-cur .docs-list"),
+						var targets = self.target.find(".content-current .docs-list"),
 							length = targets.length;
 
 						if( length != 0 ){
@@ -1412,20 +1385,20 @@ $(document).ready(function(){
 				var self = this,		
 					$filemgrContentBox = this.target.find(".frame-current .filemgr-content-box");
 				
-				this.elem_address.find("#go_button").bind("click",function(){
+				this.$address.find("#go_button").bind("click",function(){
 					var url = self.getAddress();
 					self.showFile(fixURL(url),self.getTagNameFromURL(url));
 				});
 
-				this.elem_address.find("#forword_button").bind("click",function(){
+				this.$address.find("#forword_button").bind("click",function(){
 					if(this.className == "disabled")return;
-					var currentTagId = self.target.find(".frame-current .tag-cur").attr("id");
+					var currentTagId = self.target.find(".frame-current .tag-current").attr("id");
 					self.goForword(currentTagId);
 				});
 
-				this.elem_address.find("#back_button").bind("click",function(){
+				this.$address.find("#back_button").bind("click",function(){
 					if(this.className == "disabled")return;
-					var currentTagId = self.target.find(".frame-current .tag-cur").attr("id");
+					var currentTagId = self.target.find(".frame-current .tag-current").attr("id");
 					self.goBack(currentTagId);
 				});
 
@@ -1527,25 +1500,25 @@ $(document).ready(function(){
 									fatterOS.system.$context.empty();		
 								});
 							},
-		//TODO this !!!
-		select:	function(){
+		/*select:	function(){
 									
 		},
 
 		selectAll: function(){
 							
-							},
-
+		},
+*/
 		copy: function(){
-			var targets = $(".content-cur .selected");
-		//	var targets = fatterOS.cache.select;
+		//	var targets = $(".content-current .selected");
+			var targets = fatterOS.cache.select;
 			$.each(targets,function(i){
-				fatterOS.cache.clipBoard.push(targets[i]);
+				console.log(targets[i])
+				//fatterOS.cache.clipBoard.push(targets[i]);
 			});
 		},
 
 		rm: function(){
-			var targets = $(".content-cur .selected");
+			var targets = $(".content-current .selected");
 			//var targets = fatterOS.cache.select;
 			fatterOS.confirm("确定删除这"+targets.length+"项吗？",function(value){
 				if(value){
@@ -1558,19 +1531,19 @@ $(document).ready(function(){
 
 		paste: function(){
 			var clipBoard = fatterOS.cache.clipBoard,
-				place = $(".content-cur");
+				place = $(".content-current");
 			$.each(clipBoard,function(i){
 				place.append(clipBoard[i]);
 			})
 		},
 
 		rename:	function(){
-			var targets = $(".content-cur .selected");
+			var targets = $(".content-current .selected");
 			targets.find("input").focus();
 		},
 
 		mkdir: function(){
-			var tag_id = $(".frame-current .tag-cur").attr("id"),
+			var tag_id = $(".frame-current .tag-current").attr("id"),
 				content_id = "content_" + tag_id,
 				place = $("#"+content_id),
 				name = '新建文件夹',
@@ -1584,7 +1557,7 @@ $(document).ready(function(){
 		},
 
 		mkfile: function(){
-			var tag_id = $(".frame-current .tag-cur").attr("id"),
+			var tag_id = $(".frame-current .tag-current").attr("id"),
 				content_id = "content_" + tag_id,
 				place = $("#"+content_id),
 				name = '新建文本',
@@ -1684,7 +1657,7 @@ $(document).ready(function(){
 								this.$context = fatterOS.system.$context || $("#fatterOS_context");
 								/*全局点击事件开始*/
 								this.$document.bind("mousedown.globle",function(e){
-									e.preventDefault();
+									//e.preventDefault();
 									fatterOS.eventBind.$startMenu.hide();
 									fatterOS.eventBind.$context.empty();
 									if ( !$(e.target).is('input,textarea,select') ) {
@@ -1703,8 +1676,7 @@ $(document).ready(function(){
 
 								/*全局鼠标移动事件开始*/
 								this.$document.bind("mousemove.globle",function(e){
-									e.preventDefault();
-									e.stopPropagation();
+								
 								});
 								/*全局鼠标移动事件结束*/
 
@@ -1813,14 +1785,14 @@ $(document).ready(function(){
 	fatterOS.desktop = {
 	//TODO	move those to system.prestart;
 		preload: function(){
-			var $startingContainer 	= 	$("#starting_container"),
-				$startingBar		=	$("#starting_bar"),
-				clientHeight  = fatterOS.clientInfo.clientHeight(),
-				clientWidth   = fatterOS.clientInfo.clientWidth();
+			var $preload = 	$("#preload"),
+				$startingBar = $("#starting_bar"),
+				clientHeight = fatterOS.clientInfo.clientHeight(),
+				clientWidth = fatterOS.clientInfo.clientWidth();
 
 			$startingBar.progressBar(100,{speed:25,callback:function(data){
 				if( data.running_value == data.value ){
-					$startingContainer.fadeOut("slow");
+					$preload.fadeOut("slow");
 				}
 			}});
 			return this;
